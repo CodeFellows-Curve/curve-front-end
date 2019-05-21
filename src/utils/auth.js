20fd1c43a44f318a406518c55bd5dcd2ee9c75c5
Joseph <z@charmed.tech>
2019-05-21 07:37:48 -0700

    Thin auth -> Redux handles loggedIn state

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,32 +1,50 @@
 export const isBrowser = () => typeof window !== 'undefined'
 
-export const getUser = () =>
-  isBrowser() && window.localStorage.getItem('curveUser')
-    ? JSON.parse(window.localStorage.getItem('curveUser'))
-    : {}
-
-const setUser = user =>
-  window.localStorage.setItem('curveUser', JSON.stringify(user))
-
 export const handleLogin = ({ username, password }) => {
   if (username === `john` && password === `johnny`) {
-    return setUser({
-      username: `john`,
-      name: `Johnny`,
-      email: `johnny@example.org`,
-    })
+    return true
   }
   return false
 }
 
-export const isLoggedIn = () => {
-  const user = getUser()
-  return !!user.username
-}
-
-export const logout = callback => {
-  setUser({})
+export const handleLogout = callback => {
   if (callback) {
     // callback()
   }
+  return true
 }
+
+/***
+ * Not using local storage for now because Redux is set up
+ ***
+ * export const getUser = () =>
+ *   isBrowser() && window.localStorage.getItem('curveUser')
+ *     ? JSON.parse(window.localStorage.getItem('curveUser'))
+ *     : {}
+ *
+ * const setUser = user =>
+ *   window.localStorage.setItem('curveUser', JSON.stringify(user))
+ *
+ * export const handleLogin = ({ username, password }) => {
+ *   if (username === `john` && password === `johnny`) {
+ *     return setUser({
+ *       username: `john`,
+ *       name: `Johnny`,
+ *       email: `johnny@example.org`,
+ *     })
+ *   }
+ *   return false
+ * }
+ *
+ * export const isLoggedIn = () => {
+ *   const user = getUser()
+ *   return !!user.username
+ * }
+ *
+ * export const logout = callback => {
+ *   setUser({})
+ *   if (callback) {
+ *     // callback()
+ *   }
+ * }
+ ***/

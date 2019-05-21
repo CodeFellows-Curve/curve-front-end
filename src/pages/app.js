32feb8ec10f492f73ebeb9435942584644cb0ad0
Joseph <z@charmed.tech>
2019-05-21 07:22:54 -0700

    Add dummy protected secret SWAPI route

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,26 +1,23 @@
 import React from 'react'
 import { Router } from '@reach/router'
-// import { Provider } from 'react-redux';
+
 import Layout from '../components/layout/'
 import PrivateRoute from '../components/private-route'
 import Login from '../components/login'
 import Graph from '../components/graph'
+import SecretSwapi from '../components/secret-swapi'
 
-// import createStore from "../store";
-// const store = createStore();
-// console.log('app.js line 11', store);
-
-// const Graph = () => <div>I am beautiful graph</div>
 const List = () => <div>I am very much list</div>
 
 const App = () => (
   <Layout>
-     <Router>
-        <PrivateRoute path="/app/graph" component={Graph} />
-        <PrivateRoute path="/app/list" component={List} />
-        <Login path="/app/login" />
-     </Router>
-    </Layout>
+    <Router>
+      <PrivateRoute path="/app/graph" component={Graph} />
+      <PrivateRoute path="/app/list" component={List} />
+      <PrivateRoute path="/app/secret-swapi" component={SecretSwapi} />
+      <Login path="/app/login" />
+    </Router>
+  </Layout>
 )
 
 export default App

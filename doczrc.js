064122882957bfbcdcb0bdb9e13db96ff53a8f7b
Cory Henderson <chenderson19899@gmail.com>
2019-05-22 14:41:26 -0700

    documentation for all components/reducers/actions

---
+++ doczrc.js
@@ -0,0 +1,7 @@
+export default {
+    title: 'Curve App',
+    description: 'Curve application - Joint project by jS and C#', 
+    propsParser: true,
+    __experimentalThemes: [`gatsby-theme-docz`],
+    ignore: ['**/*.{md,markdown}']
+}

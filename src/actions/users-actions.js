292ea1e3faa5b4b2d2c0baa242603da64d7a5ce7
Joseph <z@charmed.tech>
2019-05-21 17:29:48 -0700

    Add fetched users state handling

---
+++ src/actions/users-actions.js
@@ -0,0 +1,4 @@
+export const pullMarkdown = data => ({
+  data,
+  type: 'PULL_COMPETENCIES',
+})
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux'
 import auth from './auth-reducer'
 import graph from './graph-reducer'
 import competencies from './competencies-reducer'
+import users from './users-reducer'
 
-const reducers = combineReducers({ graph, auth, competencies })
+const reducers = combineReducers({ graph, auth, competencies, users })
 
 export default reducers

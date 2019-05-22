13db8ddaccea39495ce051b602dff2322f8e9c06
Joseph <z@charmed.tech>
2019-05-21 17:01:55 -0700

    Import competencies markdown files to Redux store

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,20 +1,77 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { graphql } from 'gatsby'
 import { Router } from '@reach/router'
 
+import * as a from '../actions/competencies-actions'
 import Layout from '../components/layout/'
 import PrivateRoute from '../components/private-route'
 import Login from '../components/login'
 import Graph from '../components/graph'
 import List from '../components/list'
 
-const App = () => (
-  <Layout>
-    <Router>
-      <PrivateRoute path="/app/graph" component={Graph} />
-      <PrivateRoute path="/app/list" component={List} />
-      <Login path="/app/login" />
-    </Router>
-  </Layout>
-)
+class App extends Component {
+  // set redux store to data
+  componentDidMount() {
+    const competencies = {}
 
-export default App
+    this.props.data.allMarkdownRemark.edges.forEach(num => {
+      const { category } = num.node.frontmatter
+      if (competencies[category]) {
+        const { proficiency } = num.node.frontmatter
+        if (competencies[category][proficiency]) {
+          competencies[category][proficiency].push(num.node)
+        } else {
+          competencies[category][proficiency] = [num.node]
+        }
+      } else {
+        competencies[category] = {}
+        const { proficiency } = num.node.frontmatter
+        competencies[category][proficiency] = [num.node]
+      }
+    })
+
+    this.props.pullMarkdown(competencies)
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Router>
+          <PrivateRoute path="/app/graph" component={Graph} />
+          <PrivateRoute path="/app/list" component={List} />
+          <Login path="/app/login" />
+        </Router>
+      </Layout>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  pullMarkdown: data => dispatch(a.pullMarkdown(data)),
+})
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(App)
+
+export const query = graphql`
+  {
+    allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/competencies/" } }
+    ) {
+      edges {
+        node {
+          html
+          frontmatter {
+            category
+            milestone
+            proficiency
+            title
+          }
+        }
+      }
+    }
+  }
+`
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux'
 import auth from './auth-reducer'
 import graph from './graph-reducer'
+import competencies from './competencies-reducer'
 
-const reducers = combineReducers({ graph, auth })
-
+const reducers = combineReducers({ graph, auth, competencies })
 
 export default reducers

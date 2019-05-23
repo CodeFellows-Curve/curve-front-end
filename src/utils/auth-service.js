d36085cf229530ac8a6a8eafea1682658c649708
Joseph <z@charmed.tech>
2019-05-22 23:04:18 -0700

    Consolidate auth functions to AuthService class

--- src/utils/auth-service.js
+++ src/utils/auth-service.js
@@ -1,92 +1,122 @@
 import auth0 from 'auth0-js'
-import { AUTH_CONFIG } from './auth0-config'
 import { navigate } from 'gatsby'
+import store from '../store/'
+
+import * as a from '../actions/auth-actions'
 
 const isBrowser = typeof window !== 'undefined'
 
-const auth = isBrowser
-  ? new auth0.WebAuth({
-      domain: AUTH_CONFIG.domain,
-      clientID: AUTH_CONFIG.clientID,
-      redirectUri: AUTH_CONFIG.redirectUri,
-      audience: AUTH_CONFIG.audience,
-      responseType: 'token id_token',
-      scope: 'openid profile email',
+class AuthService {
+  constructor() {
+    this.auth = isBrowser
+      ? new auth0.WebAuth({
+          domain: process.env.AUTH0_DOMAIN,
+          clientID: process.env.AUTH0_CLIENT_ID,
+          redirectUri: process.env.AUTH0_REDIRECT_URI,
+          audience: process.env.AUTH0_AUDIENCE,
+          responseType: 'token id_token',
+          scope: 'openid profile email',
+        })
+      : {}
+
+    this.tokens = {
+      accessToken: false,
+      idToken: false,
+      expiresAt: false,
+    }
+  }
+
+  handleLogin() {
+    if (!isBrowser) {
+      return
+    }
+    this.auth.authorize()
+  }
+  handleAuthentication() {
+    if (!isBrowser) {
+      return
+    }
+    console.log('handling authentication...')
+
+    this.auth.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        console.log('authResult:', authResult)
+        this.setSession()(null, authResult)
+      } else if (err) {
+        console.error(err)
+      }
     })
-  : {}
-
-const tokens = {
-  accessToken: false,
-  idToken: false,
-  expiresAt: false,
-}
-
-let user = {}
-
-export const isAuthenticated = () => {
-  if (!isBrowser) {
-    return
   }
-  return localStorage.getItem('isLoggedIn') === 'true'
-}
 
-export const login = () => {
-  console.log('THIS IS AUTH OBJECT:', auth)
-  if (!isBrowser) {
-    return
+  setSession = (cb = () => false) => (err, authResult) => {
+    console.log('Setting session with authResult:', authResult)
+    if (err) {
+      navigate('/')
+      cb()
+      return
+    }
+
+    if (authResult && authResult.accessToken && authResult.idToken) {
+      const expiresAt = authResult.expiresIn * 1000 + new Date().getTime()
+      this.tokens.accessToken = authResult.accessToken
+      this.tokens.idToken = authResult.idToken
+      this.tokens.expiresAt = expiresAt
+
+      const user = authResult.idTokenPayload
+
+      localStorage.setItem('access_token', authResult.accessToken)
+      localStorage.setItem('id_token', authResult.idToken)
+      localStorage.setItem('expires_at', expiresAt)
+      localStorage.setItem('authorized_user', user)
+
+      store.dispatch(a.login(user))
+      navigate('/app/graph')
+      return cb()
+    }
   }
-  auth.authorize()
-}
 
-const setSession = (cb = () => {}) => (err, authResult) => {
-  if (err) {
-    navigate('/')
-    cb()
-    return
+  isAuthenticated() {
+    if (!isBrowser) {
+      return
+    }
+
+    const expiresAt = JSON.parse(localStorage.getItem('expires_at'))
+    // maybe on logout set this to null
+    return new Date().getTime() < expiresAt
   }
-  if (authResult && authResult.accessToken && authResult.idToken) {
-    const expiresAt = authResult.expiresIn * 1000 + new Date().getTime()
-    tokens.accessToken = authResult.accessToken
-    tokens.idToken = authResult.idToken
-    tokens.expiresAt = expiresAt
 
-    user = authResult.idTokenPayload
-    localStorage.setItem('isLoggedIn', true)
-    navigate('/app')
-    cb()
+  getAccessToken() {
+    const accessToken = localStorage.getItem('access_token')
+    if (!accessToken) {
+      throw new Error('No access token found')
+    }
+    return accessToken
+  }
+  getTokens() {
+    return this.tokens
+  }
+
+  silentAuth(callback) {
+    if (!this.isAuthenticated()) {
+      return callback()
+    }
+    this.auth.checkSession({}, this.setSession(callback))
+  }
+
+  handleLogout() {
+    localStorage.setItem('access_token', null)
+    localStorage.setItem('id_token', null)
+    localStorage.setItem('expires_at', null)
+    localStorage.setItem('authorized_user', null)
+
+    //  const options = {
+    //    client_id: process.env.AUTH0_CLIENT_ID,
+    //    returnTo: process.env.AUTH0_DOMAIN,
+    //  }
+
+    this.auth.logout()
   }
 }
 
-export default class AuthService {
-  auth0 = auth
-
-  login() {
-    this.auth0.authorize()
-  }
-}
-export const handleAuthentication = () => {
-  if (!isBrowser) {
-    return
-  }
-  auth.parseHash(setSession())
-}
-
-export const silentAuth = callback => {
-  if (!isAuthenticated()) {
-    return callback()
-  }
-  auth.checkSession({}, setSession(callback))
-}
-
-export const getTokens = () => tokens
-
-export const getProfile = () => user
-
-export const logout = () => {
-  localStorage.setItem('isLoggedIn', false)
-  auth.logout({
-    //   client_id: process.env.AUTH0_CLIENTID,
-    //       returnTo: process.env.DOMAIN,
-    //
-  })
-}
+const authService = new AuthService()
+export default authService

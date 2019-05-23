5bece08f3d9c4af14ff439b9a4ad7f36c1ee84ca
Billy Bunn <williammbunn@gmail.com>
2019-05-22 20:21:11 -0700

    updates initial graph data, cleans up graph data processor code

--- src/utils/graph-data-processor.js
+++ src/utils/graph-data-processor.js
@@ -8,7 +8,7 @@ function toLetters(num) {
 }
 
 export default function formatGraphQLData(graphQLData) {
-  console.log('called ---------------')
+  // Creates array of objects with keys of each proficiency name and score
   let allSubcategoriesArray = graphQLData.review[0].category.reduce(
     (acc, category, idx) => {
       return (acc = [...acc, ...category.subcategory])
@@ -16,6 +16,7 @@ export default function formatGraphQLData(graphQLData) {
     []
   )
 
+  // Creates obj with key/value of each proficiency/score and calculates total points
   let milestones = allSubcategoriesArray.reduce(
     (acc, subCategory) => {
       let name = subCategory.subCategoryName.toUpperCase().replace(/ /g, '_')
@@ -26,10 +27,10 @@ export default function formatGraphQLData(graphQLData) {
     },
     { milestoneByTrack: {}, totalPoints: 0 }
   )
-  // let categoryPoints = graphQLData.review[0].category
+
+  // Creates an array of objects (1 for each competency) with the comptency name & total points for that competency
   let categoryPoints = graphQLData.review[0].category.reduce(
     (acc, category, idx) => {
-      // let categoryId = category.categoryName
       let categoryId = toLetters(idx + 1)
 
       let points = category.subcategory.reduce((acc, subcat, idx) => {
@@ -40,7 +41,7 @@ export default function formatGraphQLData(graphQLData) {
     []
   )
 
-  // OUTPUT --------------------------------------------------------------------------------
+  // OUTPUT -- Same shape as initial state in 'reducers/graph-reducer.js'
   let formattedData = {
     name: graphQLData.name,
     title: 'Hard-coded Title',
@@ -63,226 +64,227 @@ export default function formatGraphQLData(graphQLData) {
   return formattedData
 }
 
-// let exData = {
-//   name: 'Nate',
-//   review: [
-//     {
-//       category: [
-//         {
-//           categoryName: 'Business Acumen',
-//           overallScore: 1,
-//           subcategory: [
-//             {
-//               subCategoryName: 'Mission and Vision',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Customer Orientation',
-//               score: 1,
-//             },
-//           ],
-//         },
-//         {
-//           categoryName: 'Growth Mindset',
-//           overallScore: 1,
-//           subcategory: [
-//             {
-//               subCategoryName: 'Adaptability',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Curiousity',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Constant Improvement',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Handling Ambiguity',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Inclusivity',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Openness',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Ambition and Initiative',
-//               score: 1,
-//             },
-//           ],
-//         },
-//         {
-//           categoryName: 'Leadership',
-//           overallScore: 1,
-//           subcategory: [
-//             {
-//               subCategoryName: 'Accountability',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Integrity',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Ownership',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Mentorship',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Networking',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Succession',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Health and Safety',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Confidence',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Credibility',
-//               score: 1,
-//             },
-//           ],
-//         },
-//         {
-//           categoryName: 'Craft',
-//           overallScore: 1,
-//           subcategory: [
-//             {
-//               subCategoryName: 'Technical',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Process',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Innovation',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Tool Proficiency',
-//               score: 1,
-//             },
-//           ],
-//         },
-//         {
-//           categoryName: 'Quality',
-//           overallScore: 1,
-//           subcategory: [
-//             {
-//               subCategoryName: 'Judgement',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Root Cause Resolution',
-//               score: 1,
-//             },
-//           ],
-//         },
-//         {
-//           categoryName: 'Communication',
-//           overallScore: 1,
-//           subcategory: [
-//             {
-//               subCategoryName: 'Writing',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Reading',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Speaking',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Listening',
-//               score: 1,
-//             },
-//           ],
-//         },
-//         {
-//           categoryName: 'Teamwork',
-//           overallScore: 1,
-//           subcategory: [
-//             {
-//               subCategoryName: 'Collaboration',
-//               score: 1,
-//             },
-//           ],
-//         },
-//         {
-//           categoryName: 'Results',
-//           overallScore: 1,
-//           subcategory: [
-//             {
-//               subCategoryName: 'Agile',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Organizational',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Creative',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Project Execution',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Analytical Thinking',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Prioritization',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Problem Solving',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Incremental Delivery',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Decision Making',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Appropriate Autonomy',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Planning and Estimating',
-//               score: 1,
-//             },
-//             {
-//               subCategoryName: 'Dependability and Reliability',
-//               score: 1,
-//             },
-//           ],
-//         },
-//       ],
-//     },
-//   ],
-// }
-
-// console.log(formatGraphQLData(exData))
+// Example data from graphQL query
+/*
+let exData = {
+  name: 'Nate',
+  review: [
+    {
+      category: [
+        {
+          categoryName: 'Business Acumen',
+          overallScore: 1,
+          subcategory: [
+            {
+              subCategoryName: 'Mission and Vision',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Customer Orientation',
+              score: 1,
+            },
+          ],
+        },
+        {
+          categoryName: 'Growth Mindset',
+          overallScore: 1,
+          subcategory: [
+            {
+              subCategoryName: 'Adaptability',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Curiousity',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Constant Improvement',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Handling Ambiguity',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Inclusivity',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Openness',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Ambition and Initiative',
+              score: 1,
+            },
+          ],
+        },
+        {
+          categoryName: 'Leadership',
+          overallScore: 1,
+          subcategory: [
+            {
+              subCategoryName: 'Accountability',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Integrity',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Ownership',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Mentorship',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Networking',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Succession',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Health and Safety',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Confidence',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Credibility',
+              score: 1,
+            },
+          ],
+        },
+        {
+          categoryName: 'Craft',
+          overallScore: 1,
+          subcategory: [
+            {
+              subCategoryName: 'Technical',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Process',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Innovation',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Tool Proficiency',
+              score: 1,
+            },
+          ],
+        },
+        {
+          categoryName: 'Quality',
+          overallScore: 1,
+          subcategory: [
+            {
+              subCategoryName: 'Judgement',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Root Cause Resolution',
+              score: 1,
+            },
+          ],
+        },
+        {
+          categoryName: 'Communication',
+          overallScore: 1,
+          subcategory: [
+            {
+              subCategoryName: 'Writing',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Reading',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Speaking',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Listening',
+              score: 1,
+            },
+          ],
+        },
+        {
+          categoryName: 'Teamwork',
+          overallScore: 1,
+          subcategory: [
+            {
+              subCategoryName: 'Collaboration',
+              score: 1,
+            },
+          ],
+        },
+        {
+          categoryName: 'Results',
+          overallScore: 1,
+          subcategory: [
+            {
+              subCategoryName: 'Agile',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Organizational',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Creative',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Project Execution',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Analytical Thinking',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Prioritization',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Problem Solving',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Incremental Delivery',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Decision Making',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Appropriate Autonomy',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Planning and Estimating',
+              score: 1,
+            },
+            {
+              subCategoryName: 'Dependability and Reliability',
+              score: 1,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
+*/

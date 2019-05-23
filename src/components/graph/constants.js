b99153a183fa11a4c47f5b71f83112aa736206ca
abferris <abferris@gmail.com>
2019-05-23 10:48:02 -0700

    added a few notes on what I did

--- src/components/graph/constants.js
+++ src/components/graph/constants.js
@@ -44,9 +44,9 @@ export const maxLevel = 135
 
 // SOURCE: https://codefellows.github.io/common_curriculum/career_coaching/Professional_Competencies
 
+//hotfix for the extra copetency that was called __metadata make it into a temp variable, pop it, then set trackIds to be the modified verson
 const alltracks = Object.keys(tracks)
 alltracks.pop();
-
 export const trackIds = alltracks
 
 export const categoryIds = trackIds.reduce((set, trackId) => {

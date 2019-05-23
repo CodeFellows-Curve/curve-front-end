497134e6e5a5d6513401dc4395427411bd5e395d
Joseph <z@charmed.tech>
2019-05-23 15:52:35 -0700

    Convert login.js to function comp, remove button

--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 // import { navigate } from 'gatsby'
 import If from 'react-ifs'
 import Logo from './layout/logo.js'
@@ -6,23 +6,12 @@ import './layout/logo.css'
 
 import authService from '../utils/auth-service'
 
-class Login extends Component {
-  login = e => {
-    e.preventDefault()
-    authService.handleLogin()
-  }
-  render() {
-    return (
-      <If condition={!authService.isAuthenticated()}>
-        <button onClick={this.login}>Login</button>
-        <div id="logo-container">
-          <Logo />
-
-        </div>
-        
-      </If>
-    )
-  }
-}
+const Login = () => (
+  <If condition={!authService.isAuthenticated()}>
+    <div id="logo-container">
+      <Logo />
+    </div>
+  </If>
+)
 
 export default Login

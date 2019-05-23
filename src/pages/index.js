11f6a1bab0d28aec7810cc029aabf583b01ff0a2
Wei9023 <vv880923@gmail.com>
2019-05-23 13:47:00 -0700

    home page styled

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
 import Layout from '../components/layout/'
-import Login from '../components/login'
+import Login from '../components/login/login'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
-  <Layout>
+    <Layout>
+    
     <SEO title="Home" />
     <Login />
-  </Layout>
+  
+    </Layout>
+  
+    
+  
 )
 
 export default IndexPage

83e5b568e2ccda1bdc32ab95122d6f2e7be11ff3
Joseph <z@charmed.tech>
2019-05-23 19:45:42 -0700

    Implement form for adding users via mutation

--- src/components/add-user/index.js
+++ src/components/add-user/index.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react'
+import gql from 'graphql-tag'
+import { Mutation } from 'react-apollo'
+
 import './add-user.scss'
 
-//const POST_MUTATION = gql`
-//  mutation PostMutation($individual: IndividualInputType!) {
-//    addIndividual(individual: $individual) {
-//      id
-//      name
-//    }
-//  }
-//`
+const POST_MUTATION = gql`
+  mutation PostMutation($individual: IndividualInputType!) {
+    addIndividual(individual: $individual) {
+      id
+      name
+    }
+  }
+`
+
+const SubmitButton = ({ name, email }) => {
+  const individual = {
+    individual: {
+      name,
+      email,
+      competencyScore: 1,
+      currentLevel: 1,
+      pointsToNext: 10,
+    },
+  }
+  return (
+    <Mutation mutation={POST_MUTATION} variables={individual}>
+      {PostMutation => (
+        <button
+          onClick={e => {
+            PostMutation(e)
+            console.log('going')
+            e.preventDefault()
+          }}
+        >
+          Submit
+        </button>
+      )}
+    </Mutation>
+  )
+}
 
 class AddUser extends Component {
   constructor(props) {
@@ -21,13 +51,10 @@ class AddUser extends Component {
     this.setState({ [e.target.name]: e.target.value })
   }
 
-  handleSubmit = e => {
-    e.preventDefault()
-  }
-
   render() {
+    const { name, email } = this.state
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form>
         <label>
           Name:
           <input type="text" name="name" onChange={this.handleChange} />
@@ -36,80 +63,10 @@ class AddUser extends Component {
           Email:
           <input type="text" name="email" onChange={this.handleChange} />
         </label>
-        <input type="submit" value="Submit" />
+        <SubmitButton name={name} email={email} />
       </form>
     )
   }
 }
 
 export default AddUser
-//import React, { Component } from 'react'
-//import gql from 'graphql-tag'
-//import { Mutation } from 'react-apollo'
-//
-//
-//class SubmitButton extends Component {
-//  constructor(props) {
-//    super(props)
-//    this.state = {
-//      individual: {
-//      },
-//    }
-//  }
-//  render() {
-//    const { individual } = this.state
-//    return (
-//      <Mutation mutation={POST_MUTATION} variables={{ individual }}>
-//        {PostMutation => (
-//          <button
-//            onClick={e => {
-//              PostMutation(e)
-//              e.preventDefault()
-//            }}
-//          >
-//            Submit
-//          </button>
-//        )}
-//      </Mutation>
-//    )
-//  }
-//}
-//
-//export default SubmitButton
-//
-///******************************************************************/
-//// const POST_MUTATION = gql`
-////   mutation PostMutation($name: String!) {
-////     createCategory(name: $name) {
-////       _id
-////       name
-////     }
-////   }
-//// `
-////
-//// class SubmitButton extends Component {
-////   constructor(props) {
-////     super(props)
-////     this.state = {
-////       name: 'AMAAAAZING',
-////     }
-////   }
-////   render() {
-////     return (
-////       <Mutation mutation={POST_MUTATION} variables={{ name: this.state.name }}>
-////         {PostMutation => (
-////           <button
-////             onClick={e => {
-////               PostMutation(e)
-////               e.preventDefault()
-////             }}
-////           >
-////             Submit
-////           </button>
-////         )}
-////       </Mutation>
-////     )
-////   }
-//// }
-////
-//// export default SubmitButton

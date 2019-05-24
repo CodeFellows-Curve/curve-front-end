d32a717cca3a42d82d76671840b06701eecf2008
christopherknightmerritt <chris.merritt95@gmail.com>
2019-05-23 20:21:07 -0700

    add GATSBY_ to env

--- src/utils/auth-service.js
+++ src/utils/auth-service.js
@@ -20,10 +20,10 @@ class AuthService {
      ***/
     this.auth = isBrowser
       ? new auth0.WebAuth({
-          domain: process.env.AUTH0_DOMAIN || 'curveauth.auth0.com',
-          clientID: process.env.AUTH0_CLIENT_ID || '55raOQGzHseKnRVzxilQgClt7xL3oFAB',
-          redirectUri: process.env.AUTH0_REDIRECT_URI || 'http://localhost:8000/callback',
-          audience: process.env.AUTH0_AUDIENCE || 'https://curveauth/api',
+          domain: process.env.GATSBY_AUTH0_DOMAIN || 'curveauth.auth0.com',
+          clientID: process.env.GATSBY_AUTH0_CLIENT_ID || '55raOQGzHseKnRVzxilQgClt7xL3oFAB',
+          redirectUri: process.env.GATSBY_AUTH0_REDIRECT_URI || 'http://localhost:8000/callback',
+          audience: process.env.GATSBY_AUTH0_AUDIENCE || 'https://curveauth/api',
           responseType: 'token id_token',
           scope: 'openid profile email',
         })
